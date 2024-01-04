caef0583354f845ac4ca1ab97c2fa36e52308ee1
EsamMegahed <esammegahed34366@gmail.com>
2024-01-04 21:54:54 +0200

    Create listview and detail,edite,create and there templates

--- base/urls.py
+++ base/urls.py
@@ -5,5 +5,8 @@ from .import views
 
 
 urlpatterns = [
-    
+    path('',views.TaskList.as_view(),name='tasks'),
+    path('task/<int:pk>/',views.TaskDetail.as_view(),name='task_detail'),
+    path('create_task/',views.TaskCreate.as_view(),name='task_create'),
+    path('create_update/<int:pk>/',views.TaskUpdate.as_view(),name='task_update'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView,DetailView
+from django.views.generic.edit import CreateView,UpdateView
+from django.urls import reverse_lazy
+from .models import Task
 # Create your views here.
+
+
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
+    template_name = 'task_list.html'
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'base/task_detail.html'
+
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    template_name = 'base/task_form.html'
+    success_url = reverse_lazy('tasks')
+
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    template_name = 'base/task_form.html'
+    success_url = reverse_lazy('tasks')
