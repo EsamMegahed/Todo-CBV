a717f07b78fdbe06571da1fe85e17a1ec5429d58
EsamMegahed <esammegahed34366@gmail.com>
2024-01-08 23:58:29 +0200

    add registartion

--- base/urls.py
+++ base/urls.py
@@ -5,8 +5,12 @@ from .import views
 
 
 urlpatterns = [
+    # authentication
     path('login/',views.CostomLoginView.as_view(),name='login'),
-    path('logout/',views.CostomLogoutView.as_view(),name='logout'),
+    path('logout/',views.CostomLogoutView.as_view(),name='logout'), 
+    path('register/',views.RegisterPage.as_view(),name='register'), 
+
+    # base urls 
     path('',views.TaskList.as_view(),name='tasks'),
     path('task/<int:pk>/',views.TaskDetail.as_view(),name='task_detail'),
     path('create_task/',views.TaskCreate.as_view(),name='task_create'),
--- base/views.py
+++ base/views.py
@@ -1,11 +1,13 @@
 from typing import Any
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views.generic import ListView,DetailView
-from django.views.generic.edit import CreateView,UpdateView,DeleteView
+from django.views.generic.edit import CreateView,UpdateView,DeleteView,FormView
 from django.urls import reverse_lazy
 from .models import Task
 from django.contrib.auth.views import LoginView,LogoutView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 
 # Create your views here.
@@ -19,6 +21,23 @@ class CostomLoginView(LoginView):
     def get_success_url(self) -> str:
 
         return reverse_lazy('tasks')
+    
+class RegisterPage(FormView):
+    template_name = 'base/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('tasks')
+    
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request,user)
+        return super(RegisterPage,self).form_valid(form)
+    def get(self,*args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+        return super(RegisterPage,self).get(*args, **kwargs)
+
 class CostomLogoutView(LogoutView):
     
     next_page= 'login'
